92da74835e875438a5b6c3ffda37cffa4f6e8d82
Cheolkie Kim <ckiekim@naver.com>
2024-12-02 14:46:07 +0900

    User

--- src/main/java/com/lion/demo/controller/UserController.java
+++ src/main/java/com/lion/demo/controller/UserController.java
@@ -1,12 +1,21 @@
 package com.lion.demo.controller;
 
+import com.lion.demo.entity.User;
+import com.lion.demo.service.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.time.LocalDate;
+
 @Controller
 @RequestMapping("/user")
 public class UserController {
+    @Autowired
+    private UserService userService;
 
     @GetMapping("/register")
     public String registerForm() {
