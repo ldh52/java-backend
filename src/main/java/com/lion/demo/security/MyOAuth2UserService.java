a358e101a7bdfa4e2210445f110399a221b44931
Cheolkie Kim <ckiekim@naver.com>
2024-12-05 15:01:36 +0900

    Security

--- src/main/java/com/lion/demo/security/MyOAuth2UserService.java
+++ src/main/java/com/lion/demo/security/MyOAuth2UserService.java
@@ -12,6 +12,7 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.Map;
 
 @Service
 @Slf4j
@@ -36,6 +37,7 @@ public class MyOAuth2UserService extends DefaultOAuth2UserService {
                 user = userService.findByUid(uid);
                 if (user == null) {         // 내 DB에 없으면 가입을 시켜줌
                     uname = oAuth2User.getAttribute("name");
+                    uname = (uname == null) ? "github_user" : uname;
                     email = oAuth2User.getAttribute("email");
                     profileUrl = oAuth2User.getAttribute("avatar_url");
                     user = User.builder()
@@ -62,6 +64,62 @@ public class MyOAuth2UserService extends DefaultOAuth2UserService {
                             .build();
                     userService.registerUser(user);
                     log.info("구글 계정을 통해 회원가입이 되었습니다. " + user.getUname());
+                }
+                break;
+
+            case "naver":
+                Map<String, Object> response = (Map) oAuth2User.getAttribute("response");
+                String nid = (String) response.get("id");
+                uid = provider + "_" + nid;
+                user = userService.findByUid(uid);
+                if (user == null) {         // 내 DB에 없으면 가입을 시켜줌
+                    uname = (String) response.get("nickname");
+                    uname = (uname == null) ? "naver_user" : uname;
+                    email = (String) response.get("email");
+                    profileUrl = (String) response.get("profile_image");
+                    user = User.builder()
+                            .uid(uid).pwd(hashedPwd).uname(uname).email(email)
+                            .regDate(LocalDate.now()).role("ROLE_USER").provider(provider).profileUrl(profileUrl)
+                            .build();
+                    userService.registerUser(user);
+                    log.info("네이버 계정을 통해 회원가입이 되었습니다. " + user.getUname());
+                }
+                break;
+
+            case "kakao":
+                long kid = (long) oAuth2User.getAttribute("id");
+                uid = provider + "_" + id;
+                user = userService.findByUid(uid);
+                if (user == null) {         // 내 DB에 없으면 가입을 시켜줌
+                    Map<String, String> properties = (Map) oAuth2User.getAttribute("properties");
+                    Map<String, Object> account = (Map) oAuth2User.getAttribute("kakao_account");
+                    uname = (String) properties.get("nickname");
+                    uname = (uname == null) ? "kakao_user" : uname;
+                    email = (String) account.get("email");
+                    profileUrl = (String) properties.get("profile_image");
+                    user = User.builder()
+                            .uid(uid).pwd(hashedPwd).uname(uname).email(email)
+                            .regDate(LocalDate.now()).role("ROLE_USER").provider(provider).profileUrl(profileUrl)
+                            .build();
+                    userService.registerUser(user);
+                    log.info("카카오 계정을 통해 회원가입이 되었습니다. " + user.getUname());
+                }
+                break;
+
+            case "facebook":
+                String fid = oAuth2User.getAttribute("id");    // Facebook ID
+                uid = provider + "_" + fid;
+                user = userService.findByUid(uid);
+                if (user == null) {         // 내 DB에 없으면 가입을 시켜줌
+                    uname = oAuth2User.getAttribute("name");
+                    uname = (uname == null) ? "facebook_user" : uname;
+                    email = oAuth2User.getAttribute("email");
+                    user = User.builder()
+                            .uid(uid).pwd(hashedPwd).uname(uname).email(email)
+                            .regDate(LocalDate.now()).role("ROLE_USER").provider(provider)
+                            .build();
+                    userService.registerUser(user);
+                    log.info("페이스북 계정을 통해 회원가입이 되었습니다. " + user.getUname());
                 }
                 break;
         }
