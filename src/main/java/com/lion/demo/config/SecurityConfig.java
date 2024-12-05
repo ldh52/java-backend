a5dc918709615f30db6efccad4167df12474d7e9
Cheolkie Kim <ckiekim@naver.com>
2024-12-05 11:27:53 +0900

    Security

--- src/main/java/com/lion/demo/config/SecurityConfig.java
+++ src/main/java/com/lion/demo/config/SecurityConfig.java
@@ -1,8 +1,11 @@
 package com.lion.demo.config;
 
+import com.lion.demo.security.JwtRequestFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
@@ -41,5 +44,17 @@ public class SecurityConfig {
         ;
 
         return http.build();
+    }
+
+    // JWT Filter Bean 등록
+    @Bean
+    public JwtRequestFilter jwtRequestFilter() {
+        return new JwtRequestFilter();
+    }
+
+    // Authentication Manager 빈 등록
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
---
+++ src/main/java/com/lion/demo/security/MyOAuth2UserService.java
@@ -0,0 +1,45 @@
+package com.lion.demo.security;
+
+import com.lion.demo.entity.User;
+import com.lion.demo.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class MyOAuth2UserService extends DefaultOAuth2UserService {
+    @Autowired private UserService userService;
+    @Autowired private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        String uid, email, uname;
+        String hashedPwd = bCryptPasswordEncoder.encode("Social Login");
+        User user = null;
+
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+        log.info("===getAttributes()===: " + oAuth2User.getAttributes());
+        String provider = userRequest.getClientRegistration().getRegistrationId();
+        switch (provider) {
+            case "github":
+                
+                break;
+
+            case "google":
+
+                break;
+        }
+
+
+
+
+
+        return super.loadUser(userRequest);
+    }
+}
