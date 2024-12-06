fae79aba280829e8afbc392103211f7fbcc97750
Cheolkie Kim <ckiekim@naver.com>
2024-12-06 11:52:50 +0900

    Template

--- src/main/java/com/lion/demo/config/SecurityConfig.java
+++ src/main/java/com/lion/demo/config/SecurityConfig.java
@@ -21,12 +21,10 @@ public class SecurityConfig {
         http.csrf(auth -> auth.disable())       // CSRF 방어 기능 비활성화
                 .headers(x -> x.frameOptions(y -> y.disable()))     // H2-console
                 .authorizeHttpRequests(requests -> requests
-                        .requestMatchers("/book/**", "/cart/**", //"/book/list", "/book/detail",
-                                "/mall/list", "/mall/detail",
-                                "/user/register", "/h2-console", "/demo/**",
+                        .requestMatchers("/book/list", "/book/detail",
+                                "/mall/list", "/mall/detail", "/user/register", "/h2-console", "/demo/**",
                                 "/img/**", "/js/**", "/css/**", "/error/**").permitAll()
-                        .requestMatchers(//"/book/insert", "/book/yes24",
-                                "/order/listAll",
+                        .requestMatchers("/book/insert", "/book/yes24", "/order/listAll",
                                 "/order/bookStat", "/user/delete", "/user/list").hasAuthority("ROLE_ADMIN")
                         .anyRequest().authenticated()
                 )
