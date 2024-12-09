0368acaa3e95dcf2cec540445bf4a8ecb43cc828
Cheolkie Kim <ckiekim@naver.com>
2024-12-09 12:31:11 +0900

    WebSocket

---
+++ src/main/java/com/lion/demo/websocket/WebSocketConfig.java
@@ -0,0 +1,11 @@
+package com.lion.demo.websocket;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig {
+
+}
