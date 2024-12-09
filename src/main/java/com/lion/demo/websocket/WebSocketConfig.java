fe6b31e73f0f48ffb70485a4433fba5d4747a294
Cheolkie Kim <ckiekim@naver.com>
2024-12-09 14:46:10 +0900

    WebSocket

--- src/main/java/com/lion/demo/websocket/WebSocketConfig.java
+++ src/main/java/com/lion/demo/websocket/WebSocketConfig.java
@@ -1,11 +1,24 @@
 package com.lion.demo.websocket;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 @Configuration
 @EnableWebSocket
-public class WebSocketConfig {
+public class WebSocketConfig implements WebSocketConfigurer {
+//    @Bean
+//    public EchoWebSocketHandler echoWebSocketHandler() {
+//        return new EchoWebSocketHandler();
+//    }
+    @Autowired private EchoWebSocketHandler echoWebSocketHandler;
 
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(echoWebSocketHandler, "/echo")
+                .setAllowedOrigins("*");
+    }
 }
---
+++ src/main/java/com/lion/demo/websocket/WebSocketController.java
@@ -0,0 +1,21 @@
+package com.lion.demo.websocket;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/websocket")
+public class WebSocketController {
+
+    @GetMapping("/echo")
+    public String echo() {
+        return "websocket/echo";
+    }
+
+    @GetMapping("/echo2")
+    public String echo2() {
+        return "websocket/echo2";
+    }
+}
+
